f49cf492eb7f7535dea7373a1c5e058342f35f06
wilsonong-8 <wilsonong.hs@gmail.com>
2023-08-24 05:47:11 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,6 +19,7 @@ DATABASE_URI = os.getenv(
 )
 
 BASE_URL = "/accounts"
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
 ######################################################################
@@ -34,8 +35,9 @@ class TestAccountService(TestCase):
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
-        talisman.force_https = False
         init_db(app)
+        talisman.force_https = False
+
         
 
     @classmethod
