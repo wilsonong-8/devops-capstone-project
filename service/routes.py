768a85a4eec8d3a0532feb8a4a0d816cb448d5de
wilsonong-8 <wilsonong.hs@gmail.com>
2023-08-24 05:03:21 -0400

    update routes.py

--- service/routes.py
+++ service/routes.py
@@ -62,9 +62,9 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-    ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -83,7 +83,6 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -101,7 +100,6 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -118,11 +116,9 @@ def update_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -140,8 +136,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
