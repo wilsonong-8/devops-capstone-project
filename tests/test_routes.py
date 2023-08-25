9aeb48a183e576b534d01acb40539f58feab5160
wilsonong-8 <wilsonong.hs@gmail.com>
2023-08-25 05:45:57 -0400

    added nose task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -36,9 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        talisman.force_https = False
-
-        
+        talisman.force_https = False  
 
     @classmethod
     def tearDownClass(cls):
@@ -192,3 +190,4 @@ class TestAccountService(TestCase):
     #     }
     #     for key, value in headers.items():
     #         self.assertEqual(response.headers.get(key), value)
+    
